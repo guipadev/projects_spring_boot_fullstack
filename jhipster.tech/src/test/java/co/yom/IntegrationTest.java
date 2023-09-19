2f73615400eec4316ae4c3f4a116cdc2746722c6
yadevom <yaormu@gmail.com>
2023-09-18 21:42:32 -0500

    Initial version of app generated by generator-jhipster@7.9.4

---
+++ jhipster.tech/src/test/java/co/yom/IntegrationTest.java
@@ -0,0 +1,22 @@
+package co.yom;
+
+import co.yom.AppApp;
+import co.yom.config.AsyncSyncConfiguration;
+import co.yom.config.EmbeddedSQL;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { AppApp.class, AsyncSyncConfiguration.class })
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
