7d6fd71145f39140ab0964813cbf76fd349bd1a8
yadevom <yaormu@gmail.com>
2023-10-12 22:39:12 -0500

    configuracion del mongodb

--- luigi-mongo-crud/front-angular/src/app/services/product.service.ts
+++ luigi-mongo-crud/front-angular/src/app/services/product.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/enviroments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Product } from '../model/product';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
+  productURL = environment.apiResrURL + '/product';
 
-  constructor() { }
+  // Inyectamos el httpclient
+  constructor(private httpClient: HttpClient) {}
+
+  // Observable para recibir una lista de productos
+  public list(): Observable<Product[]> {
+    return this.httpClient.get<Product[]>(this.productURL);
+  }
+
+  public detail(id: number): Observable<Product> {
+    return this.httpClient.get<Product>(this.productURL + `/${id}`);
+  }
+
+  public create(product: Product): Observable<any> {
+    return this.httpClient.post<any>(this.productURL, product);
+  }
+
+  public update(id: number, product: Product): Observable<any> {
+    return this.httpClient.put<any>(this.productURL + `/${id}`, product);
+  }
+
+  public delete(id: number): Observable<any> {
+    return this.httpClient.delete<any>(this.productURL + `/${id}`);
+  }
 }
