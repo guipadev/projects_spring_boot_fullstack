98debba59593feb7136e942740466b2d4af54500
yadevom <yaormu@gmail.com>
2024-02-08 20:41:16 -0500

    Registrar un objeto desde el formulario en AngularfRegistrar un objeto desde el formulario en Angular

--- el_CRUD_Full_Stack_SpringBoot_Angular/fullstack-angular/src/app/components/customer-add/customer-add.component.ts
+++ el_CRUD_Full_Stack_SpringBoot_Angular/fullstack-angular/src/app/components/customer-add/customer-add.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Customer } from 'src/app/models/customer';
+import { CustomerService } from 'src/app/services/customer.service';
 
 @Component({
   selector: 'app-customer-add',
@@ -10,12 +12,25 @@ export class CustomerAddComponent implements OnInit {
    * Var para realizar binding con los elementos HTML
    * Que van a estar en la plantilla, cada archivo ts sabe tener su plantilla
    */
-  id: string = '';
+  id: number = 0;
   firstName: string = '';
   lastName: string = '';
   email: string = '';
 
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
+  constructor(private customerService: CustomerService) {}
+
+  ngOnInit(): void {}
+
+  addCustomer() {
+    let customer = new Customer(
+      this.id,
+      this.firstName,
+      this.lastName,
+      this.email
+    );
+    //console.log(customer);
+    this.customerService
+      .createCustomer(customer)
+      .subscribe((res) => console.log(res));
   }
 }
