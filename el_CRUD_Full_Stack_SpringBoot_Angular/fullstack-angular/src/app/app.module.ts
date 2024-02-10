06554c489d4098444a50c2c28b11283efbc80f83
yadevom <yaormu@gmail.com>
2024-02-09 20:49:37 -0500

    rutas angular

--- el_CRUD_Full_Stack_SpringBoot_Angular/fullstack-angular/src/app/app.module.ts
+++ el_CRUD_Full_Stack_SpringBoot_Angular/fullstack-angular/src/app/app.module.ts
@@ -6,11 +6,22 @@ import { AppComponent } from './app.component';
 import { CustomerListComponent } from './components/customer-list/customer-list.component';
 import { CustomerAddComponent } from './components/customer-add/customer-add.component';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', component: CustomerListComponent }, // http:localhost:4200
+  { path: 'customers/add', component: CustomerAddComponent }, // http:localhost:4200/customers/add
+];
 
 @NgModule({
   declarations: [AppComponent, CustomerListComponent, CustomerAddComponent],
-  imports: [BrowserModule, HttpClientModule, FormsModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(routes),
+  ],
   providers: [],
-  bootstrap: [CustomerAddComponent],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
